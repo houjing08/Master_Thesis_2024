62ccce32d855ff4075f7c48522b9178e72e8d1b1
bawfeh <116575951+bawfeh@users.noreply.github.com>
2024-06-27 15:21:46 +0200

    appropriate docstrings added
    All U-Net and F-Net variants and HED are implemented with these Python files

---
+++ Final/custom_classes/Unet_like.py
@@ -0,0 +1,113 @@
+from custom_classes_defs.setup import *
+
+## ===================================================
+class UNET2D(model_config):
+    """
+    - Keras 2D image segmentation with a U-Net-like architecture;
+    - Taken from https://keras.io/examples/vision/oxford_pets_image_segmentation/
+    and modified.
+    """
+    def __init__(self, panel_sizes, model_arch=None, drop_rate=0.25, **kwargs):
+        """
+        @params:
+            panel_sizes     : list of filter sizes at each resolution level
+            model_arch  : dictionaray representing other model architectures
+            drop_rate   : probability used for dropout layers
+        """
+        if model_arch is None:
+            super().__init__(**kwargs)
+        else:
+            super().__init__(**model_arch, **kwargs)
+
+        self.Name = 'unet'
+        self.panel_sizes = panel_sizes
+        self.drop_rate = drop_rate
+
+
+    def compute_zero_padding(self):
+        """ Assumes image shape is square;
+        determines padding of initial image so that subsequent images 
+        get halved each time an encoding step is applied """
+        pad  = 0   
+        L = len(self.panel_sizes)
+        zero_paddding = True
+        while zero_paddding:
+            N = self.img_shape[0] + 2*pad 
+            # infer all possible image sizes
+            img_sizes = [N // pow(2, n) for n in range(L) if pow(2,n)<N]
+            idx = np.mod(img_sizes[:-1], img_sizes[1:]).argmax()
+            newL = len(img_sizes)
+            if not(idx==0 and newL==L):
+                pad = pad + 1
+            else:
+                zero_paddding = False
+        return pad
+    
+    def build_model(self):
+        inputs = keras.Input(shape=self.img_shape + (self.channels_dim[0],))
+
+        pad = self.compute_zero_padding()
+        inter_inputs = self.take_off(inputs, pad)
+
+        ### [First half of the network: downsampling inputs] ###
+
+        # Entry block
+        x = layers.Conv2D(self.panel_sizes[0], 3, strides=2, padding="same")(inter_inputs)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+
+        previous_block_activation = x  # Set aside residual
+
+        # Blocks 1, 2, 3 are identical apart from the feature depth.
+        for filters in self.panel_sizes[1:]:
+            # if filters < self.panel_sizes[0]:
+            x = layers.Activation("relu")(x)
+            x = layers.Dropout(self.drop_rate)(x)
+            x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.Activation("relu")(x)
+            x = layers.Dropout(self.drop_rate)(x)
+            x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+            # Project residual
+            residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+                previous_block_activation
+            )
+            x = layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        ### [Second half of the network: upsampling inputs] ###
+
+        for filters in self.panel_sizes[::-1]:
+            x = layers.Activation("relu")(x)
+            x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.Activation("relu")(x)
+            x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.UpSampling2D(2)(x)
+
+            # Project residual
+            residual = layers.UpSampling2D(2)(previous_block_activation)
+            residual = layers.Conv2D(filters, 1, padding="same")(residual)
+            x = layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        x = self.landing(x, pad)
+        # Add a per-pixel classification layer
+        activation="softmax" if self.channels_dim[1]>1 else "sigmoid"
+        outputs = layers.Conv2D(
+            self.channels_dim[1], 3, 
+            activation=activation, 
+            padding="same"
+        )(x)
+
+        return keras.Model(inputs, outputs, name="U-NET")
+    
+    
---
+++ Final/custom_classes/hed.py
@@ -0,0 +1,100 @@
+from custom_classes_defs.setup import *
+
+import re
+
+## ===================================================
+class HED2D(model_config):
+    """
+        Holistic Edge Detection (2D neural network for semamtic edge detection);
+        Cf. Xie & Tu (2015);
+        Uses VGG16 as backbone network;
+    """
+    def __init__(self,  vgg16_path=None, num_freeze=4, model_arch=None, **kwargs):
+        """
+        @params:
+            vgg16_path : path to pretrained model (vgg16)
+            num_freeze : number of convolutional base blocks to freeze (0-5)
+            model_arch : other model architecture parameters
+        """
+        if model_arch is None:
+            super().__init__(**kwargs)
+        else:
+            super().__init__(**model_arch, **kwargs)
+
+        self.vgg16_path = vgg16_path
+        self.num_freeze =  min(5, max(0, int(num_freeze)))
+        self.Name = 'hed'
+
+
+    def compute_zero_padding(self):
+        """ Assumes image shape is square;
+        determines padding of initial image so that subsequent images 
+        get halved each time an encoding step is applied """
+        pad  = 0   
+        L = 5
+        zero_paddding = True
+        while zero_paddding:
+            N = self.img_shape[0] + 2*pad 
+            # infer all possible image sizes
+            img_sizes = [N // pow(2, n) for n in range(L) if pow(2,n)<N]
+            idx = np.mod(img_sizes[:-1], img_sizes[1:]).argmax()
+            newL = len(img_sizes)
+            if not(idx==0 and newL==L):
+                pad = pad + 1
+            else:
+                zero_paddding = False
+        return pad
+    
+    def build_model(self):
+        inputs = keras.Input(shape=self.img_shape + (self.channels_dim[0],))
+
+        intermediate = False
+        if self.channels_dim[0] == 1:
+            inter_inputs = layers.Concatenate(axis=-1)([inputs]*3)
+            intermediate = True
+
+        pad = self.compute_zero_padding()
+        inter_inputs = self.take_off(inter_inputs if intermediate else inputs, pad)
+        preprocessed = keras.applications.vgg16.preprocess_input(inter_inputs)
+
+        vgg16 = keras.applications.VGG16(
+            include_top=False,
+            weights="imagenet" if (self.vgg16_path is None) 
+                                else self.vgg16_path, 
+            input_tensor=preprocessed,
+        )
+
+        conv_blocks = []
+        conv_names = ['block1_conv2','block2_conv2','block3_conv3','block4_conv3','block5_conv3']
+        for i, name in enumerate(conv_names):
+            x = vgg16.get_layer(name=name).output
+            x = layers.Conv2D(1, 1, padding="same")(x)
+            if i>1:
+                x = layers.Conv2DTranspose(1, 1, strides=2**i, padding="same")(x)
+            x = self.landing(x, pad)
+            conv_blocks.append(x)
+
+        x = layers.concatenate(conv_blocks)
+        x = layers.Conv2D(1, 1, padding="same")(x)
+        x = self.landing(x, pad)
+        conv_blocks.append(x)
+
+        # Add a per-pixel classification layer
+        outputs = [
+            layers.Conv2D(
+            self.channels_dim[1], 3, 
+            activation="sigmoid", 
+            padding="same", 
+            name=f'output{i}')(x) \
+            for i, x in enumerate(conv_blocks)
+        ]
+
+        model =  keras.Model(inputs, outputs, name="HED")
+
+        # Enforce transfer learning
+        for ly in model.layers:
+            if re.search(f'block[0-{self.num_freeze}]', ly.name):
+                ly.trainable = False
+
+        return model
+    
